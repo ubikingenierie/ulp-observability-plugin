f25cc2ba9f4e70bf4fb5db971470023c17eaf8c7
jugurthak0 <j.kebir@ubik-ingenierie.com>
2022-03-24 10:15:06 +0100

    setup: setting up the develop branch

--- src/main/java/apache/jmeter/ulpobservability/listener/gui/ULPObservabilityGui.java
+++ src/main/java/apache/jmeter/ulpobservability/listener/gui/ULPObservabilityGui.java
@@ -8,22 +8,20 @@ import org.apache.jmeter.testelement.TestElement;
 public class ULPObservabilityGui extends AbstractSamplerGui{
 
 	public TestElement createTestElement() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	public String getLabelResource() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
-	public void modifyTestElement(TestElement arg0) {
-		// TODO Auto-generated method stub
+	public void modifyTestElement(TestElement testElement) {
+		
 		
 	}
 	
 	
-	
-
 }
 
