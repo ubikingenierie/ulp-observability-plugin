c22ea69a22ca9cfa409779b66bd26ddc0e8b50ce
Réda Id-taleb <idtalebreda@gmail.com>
2023-05-23 09:49:47 +0200

    create an object to store the errors raised by samples

---
+++ back/src/main/java/com/ubikloadpack/jmeter/ulp/observability/util/ErrorsMap.java
@@ -0,0 +1,118 @@
+package com.ubikloadpack.jmeter.ulp.observability.util;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+public class ErrorsMap {
+	private ConcurrentHashMap<String, ErrorTypeInfo> errorsPerType;
+	private Long totalThreads;
+	private Long totalErrors;
+	
+	public ErrorsMap() {
+		this.errorsPerType = new ConcurrentHashMap<>();
+		this.totalErrors = 0L;
+		this.totalThreads = 0L;
+	}
+
+	/**
+	 * Add an error type and increment it's occurrence.
+	 * @param errorType the error type to add.
+	 * @return The information of the added error type.
+	 */
+	public ErrorTypeInfo addErrorTypeAndCount(String errorType) {
+		ErrorTypeInfo errorTypeInfo = this.errorsPerType.computeIfAbsent(errorType, s -> new ErrorTypeInfo(errorType));
+	    errorTypeInfo.increment();
+	    return errorTypeInfo;
+	}
+	
+	/**
+	 * Compute the frequency of that error type.
+	 * @param errorType the type of error for which the frequency will be calculated
+	 * @param totalErrors the total errors. Should get it's value from the Micrometer registry.
+	 * @return Percentage of occurrences of a specific error type out of the total number of errors.
+	 * Returns null if no errorType key was found.
+	 */
+	public Double computeErrorTypeFrequency(String errorType, Long totalErrors) {
+		if (this.errorsPerType.contains(errorType)) {
+			return errorsPerType.get(errorType).computeErrorTypeFrequency(totalErrors);
+		}
+		return null;
+	}
+	
+	/**
+	 * Compute error rate of that error type.
+	 * @param errorType the type of error for which the error rate will be calculated.
+	 * @param totalRequests the total requests. Should get it's value from the Micrometer registry.
+	 * @return percentage of queries that raised an error of this type. Returns null if no errorType 
+	 * key was found.
+	 */
+	public Double computeErrorRateForType(String errorType, Long totalRequests) {
+		if (this.errorsPerType.contains(errorType)) {
+			return errorsPerType.get(errorType).computeErrorRate(totalRequests);
+		}
+		return null;
+	}
+	
+	/**
+	 * Build an ErrorsMap that contains the X top Errors.
+	 * @param maxErrors The maximum type errors to keep.
+	 * @return a new ErrorMap with only the top X Errors.
+	 */
+	public ErrorsMap collectTopXErrors(int maxErrors) {
+	    Map<String, ErrorTypeInfo> topErrors = this.errorsPerType.entrySet()
+	            .stream()
+	            .sorted(Map.Entry.<String, ErrorTypeInfo>comparingByValue().reversed())
+	            .limit(maxErrors)
+	            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+	    ErrorsMap result = new ErrorsMap();
+	    result.errorsPerType = new ConcurrentHashMap<>(topErrors);
+	    return result;
+	}
+	
+	/**
+	 * Get the openMetric format of the error types.
+	 * @param sampleName the name of the sample (should be total_label)
+	 * @param totalThreads the number of the total threads
+	 * @param totalErrors
+	 * @param timeStamp 
+	 * @return
+	 */
+	public String toOpenMetric(String sampleName, Long totalThreads, Long totalErrors, Long timeStamp) {
+		StringBuilder str = new StringBuilder();
+		
+		this.errorsPerType.forEach((errorType, errorInfo) -> {
+			Double errorFrequency = errorInfo.computeErrorTypeFrequency(totalErrors);
+			Double errorRate = errorInfo.computeErrorRate(totalThreads);
+			
+			String metric = String.format("%s_total{count=\"error_every_periods\",type=\"%s\",errorRate=\"%s\",freq=\"%s\"}", sampleName, errorType, errorRate, errorFrequency);
+			str.append(metric + " " + errorInfo.getOccurence() + " " + timeStamp +"\n");
+		});
+		return str.toString();
+	}
+
+	/**
+	 * @return the error map
+	 */
+	public ConcurrentHashMap<String, ErrorTypeInfo> getErrorPerType() {
+		return errorsPerType;
+	}
+	
+	public Long getTotalThreads() {
+		return totalThreads;
+	}
+	
+
+	public void setTotalThreads(Long totalThreads) {
+		this.totalThreads = totalThreads;
+	}
+
+	public Long getTotalErrors() {
+		return totalErrors;
+	}
+
+	public void setTotalErrors(Long totalErrors) {
+		this.totalErrors = totalErrors;
+	}
+}
