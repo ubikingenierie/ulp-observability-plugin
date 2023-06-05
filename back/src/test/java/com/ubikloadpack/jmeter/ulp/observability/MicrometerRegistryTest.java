f1d9cf2ca2b8c0157ccbd3286037793e920e187d
Réda Id-taleb <idtalebreda@gmail.com>
2023-06-05 08:08:55 +0200

    verify the value of the metrics when several samples

--- back/src/test/java/com/ubikloadpack/jmeter/ulp/observability/MicrometerRegistryTest.java
+++ back/src/test/java/com/ubikloadpack/jmeter/ulp/observability/MicrometerRegistryTest.java
@@ -37,6 +37,7 @@ public class MicrometerRegistryTest {
 	// Test the value of the metrics when one request of a sample is sent with one virtual user.
 	@Test 
 	public void testComputedMetrics_when_OneVirtualUser_and_onlyOneSampleIsSent() {
+		// ### SetUp ###
 		long responseTime = 500L; // Fix response time of that sample at 500 milliseconds
 		int groupThreads = 1;
 		Long startTime = System.currentTimeMillis();
@@ -46,24 +47,44 @@ public class MicrometerRegistryTest {
 		micrometerRegistry.addResponse(responseResult);
 		Date creationDate = new Date();
 		
-		// Testing 
+		// ### Testing ###
 		SampleLog sampleLog = micrometerRegistry.makeLog("spl_sample", creationDate);
 		SampleLog totalLabelLog = micrometerRegistry.makeLog(Util.makeMicrometerName(TOTAL_lABEL), creationDate);
 		
-		// Assertions
+		// ### Assertions ###
 		// Only one sample was recorded so the value of different percentiles are the same as the value of the response time
 		Map<Integer, Long> expectedPct = Map.of(50, responseTime, 90, responseTime, 95, responseTime); 
-		assertSampleLogValuesWhenIntervalPeriods(sampleLog, creationDate, 1, responseTime, responseTime, responseTime, 0, expectedPct, (double) 1/LOG_FREQUENCY, groupThreads);
-		assertSampleLogValuesWhenIntervalPeriods(totalLabelLog, creationDate, 1, responseTime, responseTime, responseTime, 0, expectedPct, (double) 1/LOG_FREQUENCY, groupThreads);	
+		double expectedThroughput = (double) 1/LOG_FREQUENCY; // number of requests of a sample per the log period
+		
+		// Assert the value of metrics for a log period
+		assertPeriodMetricsForSampleLog(sampleLog, creationDate, 1, responseTime, responseTime, responseTime, 0, expectedPct, expectedThroughput, groupThreads);
+		assertPeriodMetricsForSampleLog(totalLabelLog, creationDate, 1, responseTime, responseTime, responseTime, 0, expectedPct, expectedThroughput, groupThreads);	
+		
+		// Assert the value of metrics for a every periods
+		expectedThroughput = sampleLog.getSamplerCountTotal() / millisToSeconds(endTime - startTime);
+		assertEveryPeriodsMetricsForSampleLog(sampleLog, creationDate, 1, responseTime, responseTime, responseTime, 0, expectedPct, expectedThroughput, groupThreads);
+		assertEveryPeriodsMetricsForSampleLog(totalLabelLog, creationDate, 1, responseTime, responseTime, responseTime, 0, expectedPct, expectedThroughput, groupThreads);	
 	}
 	
 	@Test
 	public void testSeveralRequestOfSample() {
+		int groupThreads = 10;
+				
+		for (int i = 0; i < groupThreads; i++) {	// we add the response of 10 sequential queries of a sample.	
+			long startTime = i;
+			long endTime = startTime + 1000;
+			long responseTime = endTime - startTime;
+			
+			ResponseResult responseResult = new ResponseResult("groupe1", responseTime, false, groupThreads, 1, "sample", startTime, endTime);
+			micrometerRegistry.addResponse(responseResult);
+		}
 		
+		Date creationDate = new Date();
 	}
 	
 	/**
-	 * Checks if the value of the metrics of the given sampleLog are the same as the expected values.
+	 * Checks if the metric values ​​of the given SampleLog are the same as the expected values.
+	 * Note: This method only checks the metrics corresponding to a specific period.
 	 * @param sampleLog the sample Log for which the values ​​of the metrics will be compared to those expected.
 	 * @param expectedCreationDate the expected creation date of the sample log.
 	 * @param expectedSamplerCount the expected count of the sample.
@@ -75,23 +96,59 @@ public class MicrometerRegistryTest {
 	 * @param expectedThroughput the expected throughput (number of requests of that sample per log frequency)
 	 * @param expectedGroupThreads the expected number of virtual users of the group related to this sample.
 	 */
-	private void assertSampleLogValuesWhenIntervalPeriods(SampleLog sampleLog, Date expectedCreationDate, long expectedSamplerCount, long expectedSum, long expectedAvg, long expectedMax,
+	private void assertPeriodMetricsForSampleLog(SampleLog sampleLog, Date expectedCreationDate, long expectedSamplerCount, long expectedSum, long expectedAvg, long expectedMax,
 			 					int expectedErrors, Map<Integer, Long> expectedPcts, double expectedThroughput, int expectedGroupThreads) {
 		assertNotNull(sampleLog);
 		assertEquals(sampleLog.getTimeStamp(), expectedCreationDate);
-		assertEquals(sampleLog.getSamplerCount(), expectedSamplerCount); // One request of that sample is created
-		assertEquals(sampleLog.getError(), expectedErrors); // no errors for that sample
+		assertEquals(sampleLog.getSamplerCount(), expectedSamplerCount); 
+		assertEquals(sampleLog.getError(), expectedErrors); 
 		
 		assertTrue(sampleLog.getPct().length == 3);
-		for (ValueAtPercentile pct: sampleLog.getPct()) {
+	 	for (ValueAtPercentile pct: sampleLog.getPct()) {
 			assertEquals((long) pct.value(), expectedPcts.get((int)(pct.percentile()*100))); 
 		}
 		
 		assertEquals(sampleLog.getSum(), expectedSum);
 		assertEquals(sampleLog.getAvg(), expectedAvg); 
 		assertEquals(sampleLog.getMax(), expectedMax);
-		assertEquals(sampleLog.getThroughput(), expectedThroughput); // throughput for an interval is the number of samples per log frequency
+		assertEquals(sampleLog.getThroughput(), expectedThroughput); 
 		assertEquals(sampleLog.getThreads(), expectedGroupThreads); 
+	}
+	
+	/**
+	 * Checks if the metric values ​​of the given SampleLog are the same as the expected values.
+	 * Note: This method only checks the metrics corresponding to a every periods.
+	 * @param sampleLog the sample Log for which the values ​​of the metrics will be compared to those expected.
+	 * @param expectedCreationDate the expected creation date of the sample log.
+	 * @param expectedSamplerCount the expected count of the sample.
+	 * @param expectedSum the expected sum of response time.
+	 * @param expectedAvg the expected average of response time.
+	 * @param expectedMax the expected maximum response time.
+	 * @param expectedErrors the expected number of the errors raised by the sample.
+	 * @param expectedPcts a Map that contains for each expected percentiles it's expected value.
+	 * @param expectedThroughput the expected throughput (number of requests of that sample per log frequency)
+	 * @param expectedGroupThreads the expected number of virtual users of the group related to this sample.
+	 */
+	private void assertEveryPeriodsMetricsForSampleLog(SampleLog sampleLog, Date expectedCreationDate, long expectedSamplerCount, long expectedSum, long expectedAvg, long expectedMax,
+				int expectedErrors, Map<Integer, Long> expectedPcts, double expectedThroughput, int expectedGroupThreads) {
+		assertNotNull(sampleLog);
+		assertEquals(sampleLog.getSamplerCountTotal(), expectedSamplerCount); 
+		assertEquals(sampleLog.getErrorTotal(), expectedErrors); 
+		
+		assertTrue(sampleLog.getPctTotal().length == 3);
+		for (ValueAtPercentile pct: sampleLog.getPctTotal()) {
+			assertEquals((long) pct.value(), expectedPcts.get((int)(pct.percentile()*100))); 
+		}
+		
+		assertEquals(sampleLog.getSum(), expectedSum);
+		assertEquals(sampleLog.getAvgTotal(), expectedAvg); 
+		assertEquals(sampleLog.getMaxTotal(), expectedMax);
+		assertEquals(sampleLog.getThroughputTotal(), expectedThroughput); 
+		assertEquals(sampleLog.getThreadsTotal(), expectedGroupThreads); 
+	}
+	
+	private double millisToSeconds(double time) {
+		return time / 1000d;
 	}
 	
 
