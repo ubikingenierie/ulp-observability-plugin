cdc51d8ee6b68263147abb68771c4eb1563c1598
Réda Id-taleb <idtalebreda@gmail.com>
2023-06-05 23:40:30 +0200

    add servletConfig integration tests

--- back/src/test/java/com/ubikloadpack/jmeter/ulp/observability/server/ULPObservabilityConfigServletTest.java
+++ back/src/test/java/com/ubikloadpack/jmeter/ulp/observability/server/ULPObservabilityConfigServletTest.java
@@ -1,26 +1,53 @@
-package com.ubikloadpack.jmeter.ulp.observability;
+package com.ubikloadpack.jmeter.ulp.observability.server;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.jmeter.util.JMeterUtils;
 import org.eclipse.jetty.http.HttpStatus;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 public class ULPObservabilityConfigServletTest extends AbstractConfigTest{
 	@Test
-	public void testDoGet() throws MalformedURLException, IOException {	
-		HttpURLConnection http = (HttpURLConnection)new URL("http://localhost:8080/config").openConnection();
+	public void testDoGet() throws IOException, URISyntaxException, InterruptedException {					
+		HttpURLConnection http = (HttpURLConnection) new URL(this.endpoint, "/config").openConnection();
         http.connect();
         assertEquals(http.getResponseCode(), HttpStatus.OK_200);
         assertEquals(http.getContentType(), "application/json");
+	}
+	
+	@Test
+	public void testConfiguationContent() throws MalformedURLException, IOException {		
+		HttpURLConnection http = (HttpURLConnection) new URL(this.endpoint, "/config").openConnection();
+        http.connect();
+        assertEquals(http.getResponseCode(), HttpStatus.OK_200);
+        assertEquals(http.getContentType(), "application/json");
+        
+        String receivedConfig = getServerResponse(http);
+        String expectedConfig = "{\"metricsRoute\":\"/ulp-o-metrics\",\"logFrequency\":1,\"totalLabel\":\"total_info\",\"localeLang\":\"en\"}";
+        
+        assertEquals(receivedConfig, expectedConfig);
+        
+        listener.stopJettyServer();
+        listener.testEnded();
+	}
+	
+	private String getServerResponse(HttpURLConnection http) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader((http.getInputStream())));
+		String str;
+		List<String> lines = new ArrayList<>();
+	    while ((str = br.readLine()) != null) {
+	    	lines.add(str);
+	    }  
+	    return String.join("\n", lines);
 	}
 	
 
