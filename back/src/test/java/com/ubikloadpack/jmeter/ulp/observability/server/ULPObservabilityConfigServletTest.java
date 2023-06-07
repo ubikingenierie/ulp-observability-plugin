feb335c5f813551125d9647ad4843e6851a1bc2e
Réda Id-taleb <idtalebreda@gmail.com>
2023-06-07 18:14:31 +0200

    add a test to verify when the totalLabel contains spaces so the configservlet should send it as openMetric

--- back/src/test/java/com/ubikloadpack/jmeter/ulp/observability/server/ULPObservabilityConfigServletTest.java
+++ back/src/test/java/com/ubikloadpack/jmeter/ulp/observability/server/ULPObservabilityConfigServletTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.eclipse.jetty.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
+import com.ubikloadpack.jmeter.ulp.observability.util.Util;
+
 public class ULPObservabilityConfigServletTest extends AbstractConfigTest {
 	@Test
 	public void testDoGet() throws Exception {					
@@ -22,9 +24,28 @@ public class ULPObservabilityConfigServletTest extends AbstractConfigTest {
         assertEquals(httpResponse.getContentType(), "application/json");
         
         String receivedConfig = httpResponse.getResponse();
-        String expectedConfig = "{\"metricsRoute\":\"/ulp-o-metrics\",\"logFrequency\":1,\"totalLabel\":\"total_info\",\"localeLang\":\"en\"}";
+        String expectedConfig = String.format("{\"metricsRoute\":\"%s\",\"logFrequency\":%s,\"totalLabel\":\"%s\",\"localeLang\":\"en\"}",
+        									  METRICS_ROUTE, LOG_FREQUENCY, TOTAL_LABEL);
         
         assertEquals(receivedConfig, expectedConfig);
-
+	}
+	
+	@Test
+	public void testConfiguationContent_when_totalLabelContainsSpaces_so_shouldBeFormattedToOpenMetrics() throws Exception {		
+		this.listener.testEnded();
+		String totalLabel = "total label";
+		this.listener.setTotalLabel(totalLabel);
+		this.testStarted(HOST);
+		
+		HttpResponse httpResponse = this.sendGetRequest("/config");
+		
+		assertEquals(httpResponse.getResponseCode(), HttpStatus.OK_200);
+	    assertEquals(httpResponse.getContentType(), "application/json");
+	    
+	    String receivedConfig = httpResponse.getResponse();
+        String expectedConfig = String.format("{\"metricsRoute\":\"%s\",\"logFrequency\":%s,\"totalLabel\":\"%s\",\"localeLang\":\"en\"}",
+        									  METRICS_ROUTE, LOG_FREQUENCY, Util.makeOpenMetricsName(totalLabel));
+        
+        assertEquals(receivedConfig, expectedConfig);
 	}
 }
